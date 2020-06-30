eb6ded66b4d532026a3b05c4fd6616fadea4e48a
Makshuf ul Haque <makshuf@pop-os.localdomain>
2020-06-30 13:25:09 +0530

    Simple DFT 90% complete

--- DFT.cpp
+++ DFT.cpp
@@ -7,18 +7,18 @@
 typedef std::vector <std::complex <double>> complexVector;
 typedef std::complex <double> complexDouble;
 
-complexVector fft ( complexVector array ) {
-    uint32_t N = array.size();
-    uint32_t F = N;
+complexVector dft ( complexVector array ) {
+    double N = array.size();
+    double F = N;
 
     complexVector output;
     output.reserve(N);
 
-    uint32_t deltaF = 1;
-    for ( uint32_t f = 0 ; f < F ; f+deltaF ) {
+    double deltaF = 1;
+    for ( double f = 0 ; f < F ; f+deltaF ) {
         complexDouble tmp(0,0);
 
-        for ( uint32_t  n = 0; n < N ; n++ ) {
+        for ( double  n = 0; n < N ; n++ ) {
             double realMul = cos((2*M_PI*f*n)/N);
             double imagMul = sin((2*M_PI*f*n)/N);
 
@@ -33,5 +33,25 @@ complexVector fft ( complexVector array ) {
 }
 
 int main ( void ) {
+
+    double signalLength = 1000;
+    complexVector signal;
+    signal.reserve(signalLength);
+    double signalFrequency = 3.000;
+    double signalPhase = 0.000;//M_PI / 4;
+
+    for ( int i= 0 ; i < signalLength ; i++ ) {
+        double realP = cos ( ( ((2 * M_PI)/ signalLength) * (signalFrequency * static_cast<double>(i)) ) +  signalPhase  );
+        signal.push_back ( complexDouble ( realP, 0.000 ) );
+    }
+
+    complexVector result = dft ( signal );
+
+    for ( int i = 0; i<6 ; i++ ) {
+        
+    }
+
     return(0);
+
+
 }
