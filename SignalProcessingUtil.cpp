723e9eb39bbf6768ca49b8d27dfde0bafbe94eb6
ubdussamad <ubdussamad@gmail.com>
2020-08-12 18:01:06 +0530

    Hi

--- SignalProcessingUtil.cpp
+++ SignalProcessingUtil.cpp
@@ -1,13 +1,13 @@
 #include "SignalProcessingUtil.hpp"
+#include "gnuplot-iostream/gnuplot_i.hpp"
 
 
 template < typename signalArraytype>
 void dsp<signalArraytype>::LOG ( std::string message , short level ) {
-    if (level == 1 || enableDebugLog ) {
+    if (level == 1 || enableDebugLog_ ) {
         std::cout << std::time(0) << ": " << message << std::endl;
     }
 }
-
 
 template < typename signalArraytype>
 dsp <signalArraytype> :: dsp( const complexSignalVector_t<signalArraytype>& signal ) :
@@ -21,7 +21,8 @@ dsp <signalArraytype> :: dsp( const signalVector_t<signalArraytype>& signal ) {
 }
 
 template < typename signalArraytype >
-complexSignalVector_t<signalArraytype> dsp <signalArraytype> :: dft ( const signalArraytype sweepDelta ) {
+complexSignalVector_t<signalArraytype> dsp <signalArraytype> :: dft ( const signalArraytype sweepDelta,const double scaling_factor ) {
+
     const double N = inputSignal.size();
     double F = N;
     complexSignalVector_t<signalArraytype> output;
@@ -34,18 +35,58 @@ complexSignalVector_t<signalArraytype> dsp <signalArraytype> :: dft ( const sign
             double realMul = cos((2*M_PI*f*n)/N);
             double imagMul = sin((2*M_PI*f*n)/N);
             std::complex < signalArraytype > z ( realMul , -imagMul );
-            tmp += array[n] * z;
+            tmp += inputSignal[n] * z;
         }
 
-        output.push_back( tmp * dft_scaling_factor );
+        output.push_back( tmp * scaling_factor );
     }
 
     return ( output );
 }
 
 
-int main () {
-    complexSignalVector_t<double> siG;
-    dsp<double> x( siG );
+int main (int argc, char* argv[]) {
+    srand (std::time(0));
+    int signalLength = atoi(argv[1]);
+    double signalFrequency = 80.00;
+    double signalPhase = M_PI/4;
+    complexSignalVector_t<double> signal;
+    // Signal Genrator
+
+    std::vector <double> siGx , siGy;
+    for ( double i= 0 ; i < signalLength ; i++ ) {
+        siGx.push_back(i);
+        double noise_coeff = rand() % 20 + 1; // Inserting random phase noise.
+        double realP = sin ( ( ((2 * M_PI)/ signalLength) * (signalFrequency * static_cast<double>(i)) ) +  (signalPhase/noise_coeff)  );
+        realP += cos ( ( ((2 * M_PI)/ signalLength) * ( (double)40.000 * static_cast<double>(i)) ) +  (signalPhase/noise_coeff)  );
+        signal.push_back ( std::complex <double> ( realP, 0.000 ) );
+        siGy.push_back(realP);
+
+    }
+
+    std::cout << "Workin this block!" << std::endl;
+    dsp<double>::enableDebugLog(true);
+    dsp<double> x( signal );
+
+    auto X = x.dft( 0.05 , 0.1);
+
+    std::vector <double> z;
+
+    for ( double i= 0 ; i < signalLength ; i++ ) {
+        z.push_back(std::abs(X[i]));
+    }
+
+
+
+    Gnuplot g1("lines");
+    g1.set_grid();
+    g1.set_style("lines").plot_xy(siGx, siGy,         "Noisey Sginal");
+    // g1.set_style("lines").plot_xy(siGx, z,               "Filtered Signal");
+    g1.set_style("lines").plot_xy(siGx, z, "DFT");
+    g1.reset_plot();
+    wait_for_key();
+    
+
+
     return(0);
 }
--- SignalProcessingUtil.hpp
+++ SignalProcessingUtil.hpp
@@ -13,7 +13,7 @@
 template < typename signalArraytype>
 using  complexSignalVector_t  = std::vector <std::complex < signalArraytype >>;
 template < typename signalArraytype>
-using  signalVector_t   = std::complex < signalArraytype >;
+using  signalVector_t   = std::vector < signalArraytype >;
 
 
 template < typename signalArraytype>
@@ -23,7 +23,7 @@ private:
     double dummySignalFrequency; // Frequency of Signal.
     
     // Display all Log messages irrespective of the level.
-    bool enableDebugLog = true;
+    static bool enableDebugLog_;
 
     // This is a pointer to the main signal, which might of might not be stored on heap.
     complexSignalVector_t<signalArraytype> inputSignal;
@@ -39,18 +39,43 @@ private:
     
 public:
 
+    static void enableDebugLog (const bool flag) {
+        enableDebugLog_ = flag;
+        return;
+    }
+
     void LOG ( std::string message , short level );
 
     dsp( const complexSignalVector_t<signalArraytype>& signal ); 
     dsp( const signalVector_t<signalArraytype>&        signal );
     dsp( const signalArraytype*       signal, int length  );
 
-    complexSignalVector_t<signalArraytype> dsp <signalArraytype> :: dft ( const signalArraytype sweepDelta );
+    complexSignalVector_t<signalArraytype> dft ( const signalArraytype sweepDelta  , const double scaling_factor );
 
     // doubleVector_t  noiseFilter ();
     // complexVector_t dft ( complexVector_t signal );
     // doubleVector_t  dft ( doubleVector_t signal );
     // doubleVector_t dc_removal ();
 };
+
+// template < typename signalArraytype>
+// dsp<int>::enableDebugLog(false);
+
+
+void wait_for_key () {
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)  // every keypress registered, also arrow keys
+    //////cout << endl << "Press any key to continue..." << endl;
+
+    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+    _getch();
+#elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
+    std::cout << std::endl << "Press ENTER to continue..." << std::endl;
+
+    std::cin.clear();
+    std::cin.ignore(std::cin.rdbuf()->in_avail());
+    std::cin.get();
+#endif
+    return;
+}
 
 
